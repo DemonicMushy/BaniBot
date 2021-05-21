5990030b9feafff53c62ca8e04abc9e5fdd242de
damien <47481731+DemonicMushy@users.noreply.github.com>
2021-05-21 12:09:13 +0800

    Add Bani package itself to test new features of Bani

--- run.py
+++ run.py
@@ -10,21 +10,29 @@ from Bani.generation.rajat_work.qgen.generator.symsub import SymSubGenerator
 from Bani.generation.rajat_work.qgen.generator.fpm.fpm import FPMGenerator
 from Bani.generation.rajat_work.qgen.encoder.dummy import dummyEN
 from Bani.generation.rajat_work.qgen.generator.eda import EDAGenerator
+from Bani.generation.t5_paraphrase_gen.t5_paraphrase import T5Generator
 from Bani.generation.sentAug.sentAug import AUG
 
 df = pd.read_csv("./test_data/babybonusTest.csv")
+df2 = pd.read_csv("./test_data/covid19Test.csv")
 
 testData = []
+testData2 = []
 global adoptionFAQ
 global babybonusFAQ
 global comcareFAQ
 global covid19FAQ
-faqStore = './faqStore_test'
+faqStore = "./faqStore_testing"
 
 for i in range(len(df)):
     original = df.loc[i, "original"]
     re = df.loc[i, "reframed"]
     testData.append([original, re])
+
+for i in range(len(df2)):
+    original = df2.loc[i, "original"]
+    re = df2.loc[i, "reframed"]
+    testData2.append([original, re])
 
 
 def csvReader(path):
@@ -42,8 +50,9 @@ def csvReader(path):
 
 def makeFAQ():
     global faqStore
-    names = ["SymSub", "FPM", "EDA", "nlpAug"]
-    quantity = [3, 3, 3, 2]
+    names = ["SymSub", "FPM", "EDA", "nlpAug", "T5"]
+    quantity = [3, 3, 3, 2, 5]
+    # quantity = [0,0,0,0,0]
 
     generatorManager = GenerateManager(
         producers=[
@@ -51,15 +60,16 @@ def makeFAQ():
             FPMGenerator(),
             EDAGenerator(),
             AUG(),
+            T5Generator()
         ],
         names=names,
         nums=quantity,
     )
 
-    questions1, answers1 = csvReader("./data/adoption/adoption-copy.csv")
-    questions2, answers2 = csvReader("./data/babybonus/babybonus-copy.csv")
-    questions3, answers3 = csvReader("./data/comcare/comcare-copy.csv")
-    questions4, answers4 = csvReader("./data/covid19/covid19-copy.csv")
+    questions1, answers1 = csvReader("./data/adoption/adoption.csv")
+    questions2, answers2 = csvReader("./data/babybonus/babybonus.csv")
+    questions3, answers3 = csvReader("./data/comcare/comcare.csv")
+    questions4, answers4 = csvReader("./data/covid19/covid19.csv")
 
     adoptionFAQ = FAQ(name="Adoption", questions=questions1, answers=answers1)
     babybonusFAQ = FAQ(name="Baby Bonus", questions=questions2, answers=answers2)
@@ -70,6 +80,10 @@ def makeFAQ():
     babybonusFAQ.buildFAQ(generatorManager)
     comcareFAQ.buildFAQ(generatorManager)
     covid19FAQ.buildFAQ(generatorManager)
+    # adoptionFAQ.buildFAQ(None)
+    # babybonusFAQ.buildFAQ(None)
+    # comcareFAQ.buildFAQ(None)
+    # covid19FAQ.buildFAQ(None)
 
     adoptionFAQ.save(faqStore)
     babybonusFAQ.save(faqStore)
@@ -83,7 +97,7 @@ def loadFAQ():
     global comcareFAQ
     global covid19FAQ
     global faqStore
-    
+
     adoptionFAQ = FAQ(name="Adoption")
     babybonusFAQ = FAQ(name="Baby Bonus")
     comcareFAQ = FAQ(name="ComCare")
@@ -100,21 +114,24 @@ loadFAQ()
 
 bot = Bani(
     FAQs=[adoptionFAQ, babybonusFAQ, comcareFAQ, covid19FAQ],
-    modelPath="./latest_model3",
+    modelPath=None,
 )
 
 
 # "batchHardTriplet", "contrastiveLoss", "tripletLoss", "softmaxLayerLoss"
-# bot.train("./", epochs=5, batchSize=64, lossName="batchHardTriplet")
 
-# bot.saveModel("./latest_model3")
+bot.train("./model_testing", epochs=5, batchSize=64, lossName="batchHardTriplet")
 
+bot.saveModel("./model_testing")
 
-acc = bot.test(1, testData, i)
+acc = bot.test(1, testData)
+acc2 = bot.test(3, testData2)
+
 print(acc)
+print(acc2)
 
-ans = bot.findClosest("how is the weather today")
+# ans = bot.findClosest("how is the weather today")
 
-for i in range(4):
-    print(ans[i].maxScore)
-    print(ans[i].score)
+# for i in range(4):
+#     print(ans[i].maxScore)
+#     print(ans[i].score)
